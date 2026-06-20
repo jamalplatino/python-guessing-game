fbc63c6bcc10a7cadb1ea5a038650aa6fba5f565
unknown <jamalplatino01.com>
2026-06-20 21:17:28 +0100

    guessing game update

--- number_guessing.py
+++ number_guessing.py
@@ -6,7 +6,6 @@ chosenNumber = random.choice([1,2,3,4,5,6,7,8,9,10])
 choice = input("Pick a number between 1-10 for your lucky number: ")
 
 while True:
-    print(chosenNumber)
     # higher than
     if int(choice) > chosenNumber:
         choice = input(f"Too high, try again but choose a lower number than {choice} ")
